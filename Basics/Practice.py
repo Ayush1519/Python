8933f3edcfd034faf9beb677d7a94e88768aa258
Ayush1519 <singhayush6872@gmail.com>
2026-02-25 23:15:54 +0530

    Add basic arithmetic and variable swapping example
    Implement basic arithmetic operations and variable swapping in Python.

---
+++ Basics/Practice.py
@@ -0,0 +1,34 @@
+print("Hello, World!")
+a=10
+b=20
+c,d =30,40
+print(c)
+print(d)
+print(a+b)
+print("Before swapping:")
+print(a)
+print(b)
+a=a+b
+b=a-b
+a=a-b
+print("After swapping:")   
+print(a)
+print(b)
+e=int(input("Enter a number: "))
+f=int(input("Enter another number: "))
+print("The sum is:", e+f)       
+print("The difference is:", e-f)
+print("The product is:", e*f)   
+print("The division is:", e/f)
+print("The modulus is:", e%f)
+print("The floor division is:", e//f)
+print(a==2)
+m=int(input("Enter the value of m"))
+n=int(input("Enter the value of n"))
+z=int(input("Enter which operation you want to perform from 1-6"))
+if(z==1):
+    print("Addition is:",m+n)
+elif(z==2):
+    print("Sunstraction:", m-n)
+elif(z==3):
+    print("")
