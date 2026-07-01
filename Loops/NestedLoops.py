21f3fffdcd07694af484f1c883b32fc5be8bec65
Ayush1519 <singhayush6872@gmail.com>
2026-07-01 23:59:04 +0530

    Comments

--- Loops/NestedLoops.py
+++ Loops/NestedLoops.py
@@ -1,5 +1,5 @@
 print("Nested loop:")
 for i in range(1, 4): #using for loop to iterate
     for j in range(1, 4):
-        print(f"({i},{j})", end=" ")
+        print(f"({i},{j})",  end=" ")
     print()
